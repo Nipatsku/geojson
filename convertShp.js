770f1e3b95fd2c10eb97146b87ad28180c2a3f57
niilo <niilo.keinanen@arction.com>
2020-06-25 12:42:04 +0300

    Do it properly.

--- convertShp.js
+++ convertShp.js
@@ -6,25 +6,39 @@ var jf = require('jsonfile')
 var shpPath = './data/shp/'
 var geojsonPath = './data/geojson/'
 
-fs.readdir( shpPath , (err, files) => {
-    files.forEach(file => {
-      console.log(file);
-      const srcPath = `${shpPath}${file}`
-      shp.open( srcPath )
-          .then(source => source.read()
-              .then(function log(result) {
-                  if (result.done) return;
-                  console.log(result.value);
-                  return source.read()
-                      .then( r => {
-                          console.log( r )
-                          const outPath = `${geojsonPath}${file.replace('.shp', '.json')}`
-                          jf.writeFile(outPath, r, function() {
-                          })
-                      } )
-              }))
-          .catch(error => console.error(error.stack));
-    });
-});
+const files = fs.readdirSync( shpPath )
+console.log( files )
 
+;( async () => {
+    console.log('do de ding')
+    for ( let i = 0; i < files.length; i ++ ) {
+        const fileName = files[i]
+        const srcPath = `${shpPath}${fileName}`
+        console.log(`${srcPath}`)
 
+        const data = await new Promise( resolve => {
+            shp.open( srcPath )
+                .then( async (source) => {
+                    console.log(`\tReading...`)
+                    const data = []
+                    let iBatch = 0
+                    let result
+                    do {
+                        console.log(`\t${iBatch++}`)
+                        result = await source.read( srcPath )
+                        const value = result.value
+                        data.push( value )
+    
+                        if ( result.done ) {
+                            return resolve( data )
+                        }
+                    } while ( ! result.done )
+                } )
+        })
+
+        const outPath = `${geojsonPath}${fileName.replace('.shp', '.json')}`
+        console.log(`\tWriting ${ outPath }`)
+        jf.writeFile( outPath, data )
+
+    }
+})()
